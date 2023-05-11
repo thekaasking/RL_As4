4ac7460a8f5f0a1c9d3a47039edd7c46c4f45403
Razo van Berkel <31627398+thekaasking@users.noreply.github.com>
2023-05-11 14:29:57 +0200

    Add files via upload

---
+++ REINFORCEAgent.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Model-based Reinforcement Learning policies
+Practical for course 'Reinforcement Learning',
+Bachelor AI, Leiden University, The Netherlands
+2021
+By Thomas Moerland
+"""
+import numpy as np
+# from MBRLEnvironment import WindyGridworld
+
+class REINFORCEAgent:
+
+    def __init__(self):
+
+    def select_action(self):
+
+        return action
+        
+    def update(self):
+
+
+
+if __name__ == '__main__':
+    test()
