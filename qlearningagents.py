5d23b6b6b5a22e36e55a4149564588867bf95172
Razo van Berkel <31627398+thekaasking@users.noreply.github.com>
2023-05-25 23:23:50 +0200

    Add files via upload

--- qlearningagents.py
+++ qlearningagents.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 import scipy as sp
 import gymnasium as gym
@@ -112,35 +111,6 @@ def run_repetitions(n_episodes, alpha, epsilon):
 #         self.Q[state][action] = self.Q[state][action] + alpha * (reward+np.max(self.Q[newstate,:])-self.Q[state][action])
 
 
-class SARSAAgent(object):
-
-    def __init__(self, n_actions, n_states, epsilon):
-        self.n_actions = n_actions
-        self.n_states = n_states
-        self.epsilon = epsilon
-        self.Q = np.zeros((n_states, n_actions))
-        
-    def select_action(self, state):
-        action = np.argmax(self.Q[state,:])
-        if np.random.random() > self.epsilon:
-            return action
-        else:
-            a = np.random.choice(range(self.Q[state,:].size))
-            while a == action:
-                a = np.random.choice(range(self.Q[state,:].size))
-            action = a
-        return action
-        
-    def update(self, state, action, reward, alpha, state_1, action_1):
-        self.Q[state][action] = self.Q[state][action] + alpha*(reward + (self.Q[state_1][action_1])-self.Q[state][action])
-
-
-# def cur_state(env):
-#     # do ando an action])
-#     # empty step
-#     new_state, b, c, d, e = env.step(0)
-#     return new_state
-
 
 # def run_repetitions(n_episodes, alpha, epsilon):
 #     env = gym.make("CartPole-v1", render_mode="rgb_array")
@@ -171,32 +141,6 @@ class SARSAAgent(object):
 #     return y, rewards
 
 
-def run_repetitions_SARSA(n_episodes, alpha, epsilon):
-    env = gym.make("CartPole-v1", render_mode="rgb_array")
-    rewards = np.zeros(n_episodes)
-    
-    dim_x = len(env.observation_space.low)
-    dim_y = len(env.observation_space.high)
-
-    y = np.zeros(dim_x)
-    
-    n_actions = env.action_size.sample
-
-    pi = SARSAAgent(n_actions=n_actions, epsilon = epsilon, n_states = env.state_size())
-    for i in range(n_episodes):
-        env.reset()
-        a = pi.select_action(env.state()) # select action
-        while not env.done():
-            state = env.state()
-            r = env.step(a) # sample reward
-            action_1 = pi.select_action(env.state()) # select next action
-            y[state] = np.argmax(pi.Q[state,:])
-            pi.update(state, a, r, alpha, env.state(), action_1) # update policy
-            rewards[i] += r
-            a = action_1
-    return y, rewards
-
-
 def experiment(n_episodes, n_repetitions, smoothing_window, epsilon, alpha_values):
 
     # Assignment 1: Q-learning ---------------------------------------------------
@@ -208,16 +152,6 @@ def experiment(n_episodes, n_repetitions, smoothing_window, epsilon, alpha_value
         avgs = np.sum(results, axis = 0)/n_repetitions
         LC_Qlearning.add_curve(smooth(avgs,smoothing_window),label=f'alpha={alpha}, smoothed')
     LC_Qlearning.save(name='learning_curve_Q_learning.png')
-
-    # Assignment 2: SARSA ---------------------------------------------------
-    # LC_SARSA = LearningCurvePlot(title = "Learning curve for SARSA")
-    # for alpha in alpha_values:
-    #     results = np.zeros((n_repetitions, n_episodes))
-    #     for i in range(n_repetitions):
-    #         temp, results[i]  = run_repetitions_SARSA(n_episodes, alpha, epsilon)
-    #     avgs = np.sum(results, axis = 0)/n_repetitions
-    #     LC_SARSA.add_curve(smooth(avgs,smoothing_window),label=f'alpha={alpha}, smoothed')
-    # LC_SARSA.save(name='learning_curve_SARSA.png')
 
 
 if __name__ == '__main__':
@@ -237,5 +171,3 @@ if __name__ == '__main__':
     array_y, rewards = run_repetitions(n_episodes, alpha, epsilon)
     print(array_y)
     print(rewards)
-    
-   # array_y, rewards = run_repetitions_SARSA(n_episodes, alpha, epsilon)
